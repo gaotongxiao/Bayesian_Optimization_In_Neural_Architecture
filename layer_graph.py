594e94cc1c41ae3d59b40e4612a43b93b388020f
gt <gaotongxiao@gmail.com>
2018-04-11 22:03:53 -0500

    finished basic functions in layer_graph

--- layer_graph.py
+++ layer_graph.py
@@ -1,7 +1,7 @@
 import networkx as nx
 from enum import Enum
 
-layers = Enum('layers', ('conv3', 'conv5', 'maxpool', 'avgpool', 'fc', 'ip', 'op'))
+layers = Enum('layers', ('conv3', 'conv5', 'maxpool', 'avgpool', 'fc', 'ip', 'op', 'softmax'))
 
 class layer_graph(object):
     def __init__(self, input_unit):
@@ -10,7 +10,7 @@ class layer_graph(object):
         self.layer_count = 1
         self.input_unit = input_unit
     
-    def add_node(self, type, num_of_filters):
+    def add_node(self, type, num_of_filters=0):
         '''
         Return:
             node number
@@ -20,25 +20,55 @@ class layer_graph(object):
         return self.layer_count - 1
     
     def add_edge(self, f, t):
-        '''
-        Ignore efficiency
-        '''
         self._graph.add_edge(f, t)
-        total_filters = 0
-        for n in self._graph.predecessors(t):
-            total_filters += self._graph.node[n]['num_of_filters']
-        self._graph.node[t]['layer_mass'] = total_filters * self._graph.node[t]['num_of_filters']
     
     def get_node(self, n):
         return self._graph.node[n]
 
-
+    def update_lm(self, zeta1=0.1, zeta2=0.1):
+        '''
+        Args:
+            zeta1: for ip, op
+            zeta2: for decision layers
+        '''
+        nodes = nx.topological_sort(self._graph)
+        ipop = []
+        dl = []
+        pl_lm = 0
+        for node in nodes:
+            if self._graph.node[node]['type'] == layers.ip or self._graph.node[node]['type'] == layers.op:
+                ipop.append(node)
+            elif self._graph.node[node]['type'] == layers.softmax:
+                dl.append(node)
+            else:
+                total_filters = 0
+                for n in self._graph.predecessors(node):
+                    total_filters += self._graph.node[n]['num_of_filters']
+                self._graph.node[node]['layer_mass'] = total_filters * self._graph.node[node]['num_of_filters']
+                pl_lm += self._graph.node[node]['layer_mass']
+        for node in ipop:
+            self._graph.node[node]['layer_mass'] = zeta1 * pl_lm
+        for node in dl:
+            self._graph.node[node]['layer_mass'] = zeta2 * pl_lm / len(dl)
+            
+        
+            
 '''
 G = layer_graph(1)
 G.add_node(layers.conv3, 16)
 G.add_node(layers.conv3, 16)
 G.add_node(layers.conv3, 32)
+G.add_node(layers.softmax)
+G.add_node(layers.softmax)
+G.add_node(layers.op)
 G.add_edge(1, 3)
 G.add_edge(2, 3)
+G.add_edge(3, 4)
+G.add_edge(3, 5)
+G.add_edge(4, 6)
+G.add_edge(5, 6)
+G.update_lm()
 print(G.get_node(3))
+print(G.get_node(4))
+print(G.get_node(6))
 '''
