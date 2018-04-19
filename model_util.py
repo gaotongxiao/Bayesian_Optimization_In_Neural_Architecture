a9a978f8064966b53db23d1036fb04c5b86c410c
gt <gaotongxiao@gmail.com>
2018-04-18 23:33:04 -0500

    finished mcmc

--- model_util.py
+++ model_util.py
@@ -1,20 +1,26 @@
 from distance import get_distance
 import numpy as np
 from scipy.stats import norm
+import emcee
 
 class NetModel():
     def __init__(self):
         self.alpha = 0.1
         self.alpha_bar = 0.1
-        self.betas = [0.1, 0.2, 0.3, 0.4]
-        self.beta_bars = [0.1, 0.2, 0.3, 0.4]
-        self.v_str = [0.1, 0.2, 0.4, 0.8]
+        # self.betas = [0.1, 0.2, 0.3, 0.4]
+        self.betas = [0.1]
+        # self.beta_bars = [0.1, 0.2, 0.3, 0.4]
+        self.beta_bars = [0.1]
+        # self.v_str = [0.1, 0.2, 0.4, 0.8]
+        self.v_str = [0.1]
         self.mu = 0
+        self.num_beta = 1
+        self.num_of_paras = 4
 
     def K(self, X1, X2=None):
         def K_single(x1, x2):
-            first_term = self.alpha*np.exp(sum([-self.betas[i]*get_distance(x1, x2,self.v_str[i])[0] for i in range(4)]))
-            second_term = self.alpha_bar*np.exp(sum([-self.beta_bars[i]*get_distance(x1, x2, self.v_str[i])[1] for i in range(4)]))
+            first_term = self.alpha*np.exp(sum([-self.betas[i]*get_distance(x1, x2,self.v_str[i])[0] for i in range(self.num_beta)]))
+            second_term = self.alpha_bar*np.exp(sum([-self.beta_bars[i]*get_distance(x1, x2, self.v_str[i])[1] for i in range(self.num_beta)]))
             return first_term + second_term
         if not X2:
             X2 = X1
@@ -41,3 +47,25 @@ class NetModel():
         mu_x = self.post_mu(x, X, Y)
         K_xx = self.post_K(x, x, X)
         return (cur_max - mu_x)*norm.cdf(cur_max, mu_x, np.sqrt(K_xx)) + K_xx*norm.pdf(cur_max, mu_x, K_xx)
+
+    def mcmc(self):
+        def lnprob(p):
+            if np.any(p < 0 + p > 1):
+                return -np.inf
+            self.alpha = p[0]
+            self.alpha_bar = p[1]
+            self.betas = [p[2]]
+            self.beta_bars = [p[3]]
+            return self.likelihood(....)
+        nwalkers, ndim = 36, self.num_of_paras
+        sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob)
+
+        # Initialize the walkers.
+        p0 = 0.5 * np.ones((4)) + 1e-4 * np.random.randn(nwalkers, ndim)
+
+        print("Running burn-in")
+        p0, _, _ = sampler.run_mcmc(p0, 200)
+
+        print("Running production chain")
+        sampler.run_mcmc(p0, 200);
+    
